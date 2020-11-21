f2bf34ed32b98e796803ca16d89648dac8041030
davidbarat <dav.barat@gmail.com>
2020-11-21 23:30:37 +0100

    commit heroku

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -170,7 +170,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
+        os.path.join(PROJECT_ROOT, 'staticfiles'),
     )
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
