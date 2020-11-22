4ae4d5bace450e0321b8818bb98c56937d875c71
davidbarat <dav.barat@gmail.com>
2020-11-22 01:19:12 +0100

    test heroku static

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -165,12 +165,12 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'teststaticfiles')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
     # Extra places for collectstatic to find static files.
-    """ STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'search'),
-    ) """
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
