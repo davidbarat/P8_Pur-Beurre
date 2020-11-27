2a8ee2f9ac30af3a6f10f0d794fda896831df322
davidbarat <dav.barat@gmail.com>
2020-11-27 14:10:07 +0100

    test static

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -172,12 +172,12 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static/search'),
-    )
+    """ STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    ) """
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
