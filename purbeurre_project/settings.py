03c64b3f497fefe1499497f888b4eeae3217bdd3
davidbarat <dav.barat@gmail.com>
2020-11-29 22:53:40 +0100

    static

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -40,7 +40,7 @@ else:
     DEBUG = True
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','.herokuapp.com', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','.herokuapp.com']
 
 # Application definition
 
@@ -170,12 +170,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+""" STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+) """
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
