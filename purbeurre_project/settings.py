c2f1579ee4c53f59d00f8e671eb079db243e6251
davidbarat <dav.barat@gmail.com>
2020-12-02 00:17:52 +0100

    super static

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -179,12 +179,12 @@ STATIC_URL = '/static/'
 ) """
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+"""STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-)
+)"""
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
